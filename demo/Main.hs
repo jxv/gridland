9f49876b7e1ec3eedecd439e2a230b8332cb671a
jxv <jxv@hush.com>
2015-06-19 00:42:45 -0700

    Move demo functions to demo

--- demo/Main.hs
+++ demo/Main.hs
@@ -3,6 +3,26 @@ module Main where
 import GridLand
 
 main :: IO ()
-main = do
-    putStrLn "Welcome, to Grid Land!"
-    test
+main = runGridLand (myCfg, myStart, myUpdate, myEnd)
+
+myCfg :: Config
+myCfg = mkConfig(10, 10, 64)
+
+myStart :: GridLand () (Sprite, Int)
+myStart = do
+    bkgImg <- loadBackdropImageStretch("data/image.bmp", Pixelated)
+    backdrop bkgImg
+    backdrop $ BkdColor(Pink)
+    colorSprite <- loadSprite("data/image.bmp")
+    return(colorSprite, 0)
+
+myUpdate :: GridLand (Sprite, Int) Bool
+myUpdate = do
+    (colorSprite, n) <- getData
+    drawSpriteFront(colorSprite, Tint Blue, mkLocation(1,2), Degrees n)
+    drawSpriteBack(colorSprite, NoFilter, mkLocation(2,2), Degrees (n * 3))
+    putData (colorSprite, n + 1)
+    return(True)
+
+myEnd :: GridLand (Sprite, Int) ()
+myEnd = return ()
