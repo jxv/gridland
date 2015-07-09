5370e74978741efda7e020038e23f4597c944463
jxv <jxv@hush.com>
2015-07-09 10:29:09 -0700

    Update demo

--- demo/Main.hs
+++ demo/Main.hs
@@ -9,17 +9,19 @@ main = runGridLand cfg start update (return ())
 cfg :: Config
 cfg = Config { cfgRows = 9, cfgCols = 12, cfgTileSize = 80 }
 
+-- App :: Sprite -> Sprite -> Angle -> Location -> App
 data App = App {
     fluffy :: Sprite,
     cocoa :: Sprite,
-    angle :: Angle, -- Degrees, Radians
-    location :: Location
+    angle :: Angle, -- Degrees, Radians,
+    cocoaLocations :: [Location],
+    fluffyLocations :: [Location]
 }
 
 start :: GridLand () App
 start = do
     titleBar "The Adventures of Cocoa & Fluffy"
-    bkgImg <- loadBackdropImage "data/lion.png"
+    bkgImg <- loadBackdropImage "data/image.bmp"
     backdrop bkgImg
     cocoaSprite <- loadSprite "data/cocoa.png"
     fluffySprite <- loadSprite "data/lion.png"
@@ -29,20 +31,44 @@ start = do
             fluffy = fluffySprite,
             cocoa = cocoaSprite,
             angle = Degrees 0,
-            location = Location 0 0
+            cocoaLocations = replicate 20 (Location 0 0),
+            fluffyLocations = replicate 50 (Location 0 0)
         }
 
 update :: GridLand App ()
 update = do
+    updateAngle
+    updateCocoas
+    updateFluffies
+
+updateAngle :: GridLand App ()
+updateAngle = do
+    app <- getData
+    let (Degrees d) = angle app
+    putData app { angle = Degrees (d + 1) }
+
+updateFluffies :: GridLand App ()
+updateFluffies = do
+    app <- getData
+    inputs <- getInputs
+    let loc = move (head $ fluffyLocations app) inputs
+    let locs = if loc == (head $ fluffyLocations app)
+            then fluffyLocations app
+            else loc : (init $ fluffyLocations app)
+    let draw (loc,color) = drawSpriteMiddle (fluffy app) (Tint color) loc (angle app)
+    mapM_ draw (zip locs $ cycle [minBound .. maxBound])
+    putData app { fluffyLocations = locs }
+
+updateCocoas :: GridLand App ()
+updateCocoas = do
     app <- getData
     mpos <- getMousePosition
-    drawSpriteFront (cocoa app) (Replace Blue) mpos (angle app)
-    drawSpriteMiddle (cocoa app) NoFilter (Location 3 3) (Degrees 0)
-    drawSpriteBack (fluffy app) (Replace Yellow) (location app) (angle app)
-    let (Degrees d) = angle app
-    inputs <- getInputs
-    let loc = move (location app) inputs
-    putData app{ angle = Degrees (d + 1), location = loc }
+    let locs = if mpos == (head $ cocoaLocations app) -- Update if there's change in mouse position
+            then cocoaLocations app
+            else mpos : (init $ cocoaLocations app)
+    let draw (loc,color) = drawSpriteFront (cocoa app) (Tint color) loc (angle app)
+    mapM_ draw (zip locs $ cycle [minBound .. maxBound])
+    putData app { cocoaLocations = locs }
 
 move :: Location -> [Input] -> Location
 move loc inputs =
